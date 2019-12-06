cacfc037df36b98c2046266c28e1db96bcd75c44
Katherine Marsh <kwmarsh@ncsu.edu>
2019-12-06 11:14:18 -0500

    updated images

--- src/data/index.js
+++ src/data/index.js
@@ -2,42 +2,42 @@ export const levelData = [
     {
         num: '0', 
         description: '',
-        imgSrc: 'https://st.motortrend.com/uploads/sites/10/2015/11/2008-toyota-camry-xle-v6-sedan-side-view.png',
+        imgSrc: 'https://github.com/katherinemarsh/autonomous-vehicle-infographic/blob/master/src/images/toyotaCamry2008.png?raw=true',
         imgDesc: '2008 Toyota Camry',
         imgCitation: 'Source: Toyota.'
     },
     {
         num: '1', 
         description: '',
-        imgSrc: 'https://65e81151f52e248c552b-fe74cd567ea2f1228f846834bd67571e.ssl.cf1.rackcdn.com/Toyota/2018%20Corolla/blizzard-pearl-18.png',
+        imgSrc: 'https://github.com/katherinemarsh/autonomous-vehicle-infographic/blob/master/src/images/toyotaCorolla2018.png?raw=true',
         imgDesc: '2018 Toyota Corolla with Toyota Safety Sense',
         imgCitation: 'Source: Toyota.'
     },
     {
         num: '2', 
         description: '',
-        imgSrc: 'https://65e81151f52e248c552b-fe74cd567ea2f1228f846834bd67571e.ssl.cf1.rackcdn.com/ldm-images/2019-Volvo-XC40-Ice-White.png',
+        imgSrc: 'https://github.com/katherinemarsh/autonomous-vehicle-infographic/blob/master/src/images/volvoXC40_2019.png?raw=true',
         imgDesc: '2019 Volvo XC40 with Pilot Assist',
         imgCitation: 'Source: Volvo.'
     },
     {
         num: '3', 
         description: '',
-        imgSrc: 'https://www.audiusa.com/content/dam/audiusa/Models/A8_2020/2020-Audi-A8-063_1920.jpg',
+        imgSrc: 'https://github.com/katherinemarsh/autonomous-vehicle-infographic/blob/master/src/images/audiA8_2020.png?raw=true',
         imgDesc: '2020 Audi A8',
         imgCitation: 'Source: Audi.'
     },
     {
         num: '4', 
         description: '',
-        imgSrc: 'https://storage.googleapis.com/sdc-prod/v1/press/waymo_minivan_5_thumb.jpg',
+        imgSrc: 'https://github.com/katherinemarsh/autonomous-vehicle-infographic/blob/master/src/images/waymoMinivan.png?raw=true',
         imgDesc: 'Waymo Chrysler Pacifica Hybrid Minivan',
         imgCitation: 'Source: Waymo.'
     },
     {
         num: '5', 
         description: '',
-        imgSrc: 'https://s3-prod-europe.autonews.com/s3fs-public/styles/1152x647/public/GENEVA_AUTO_SHOW_306009996_PH_3_WWIALYFMBHUE.jpg',
+        imgSrc: 'https://github.com/katherinemarsh/autonomous-vehicle-infographic/blob/master/src/images/volkswagonGroupLevel5.png?raw=true',
         imgDesc: 'Future Volkswagon group autonomous vehicle',
         imgCitation: 'Source: Volkswagon.'
     }
