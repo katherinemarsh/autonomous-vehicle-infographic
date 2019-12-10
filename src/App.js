6159830adccbae9ba9884132f678e4193847cb2d
Katherine Marsh <kwmarsh@ncsu.edu>
2019-12-10 08:59:31 -0500

    latest

--- src/App.js
+++ src/App.js
@@ -14,8 +14,11 @@ class App extends Component {
     const {sensorItems} = this.state
     return (
       <div className="main">
-        <h2>Levels of Autonomous Driving</h2>
-        <ol>
+        <h2>Levels of Autonomous Vehicles</h2>
+        <p className="overview">Vehicles such as cars, vans, buses, and trucks can be designed with different amounts of autonomy. 
+          The level of autonomy depends the sensors and software installed in the vehicle. Read below to 
+          learn how each level operates.</p>
+        <ol className="levels">
           {levelItems.map((levelItem, i) => <LevelItem key={i} levelItem={levelItem} />)}
         </ol>
         <h2>Sensor Packages</h2>
@@ -31,10 +34,21 @@ class LevelItem extends Component {
   render() {
     const {levelItem} = this.props
     return (
-      <li className="level-item">
-        <h3>Level {levelItem.num}</h3>
-        <p>{levelItem.description}</p>
-        <img alt={levelItem.imgLabel} src={levelItem.imgSrc}/>
+      <li id="level-item" className={levelItem.num}>
+        <div className="text">
+          <h3>Level {levelItem.num}</h3>
+          <p className="label">{levelItem.description}</p>
+          <ul class="notes">
+            <li>{levelItem.note1}</li>
+            <li>{levelItem.note2}</li>
+            <li>{levelItem.note3}</li>
+          </ul>
+        </div>
+        <div className="car">
+          <img alt={levelItem.imgLabel} src={levelItem.imgSrc}/>
+          <p className="carNote">{levelItem.imgDesc}</p>
+          <p className="carNote">{levelItem.imgCitation}</p>
+        </div>
       </li>
     )
   }
--- src/data/index.js
+++ src/data/index.js
@@ -1,7 +1,10 @@
 export const levelData = [
     {
         num: '0', 
-        description: '',
+        description: 'Driver Assistance',
+        note1: 'The driver has complete control of the vehicle.',
+        note2: 'No driver assistance other than standard cruise control.',
+        note3: 'No sensors.',
         imgSrc: 'https://github.com/katherinemarsh/autonomous-vehicle-infographic/blob/master/src/images/toyotaCamry2008.png?raw=true',
         imgDesc: '2008 Toyota Camry',
         imgCitation: 'Source: Toyota.'
@@ -9,34 +12,40 @@ export const levelData = [
     {
         num: '1', 
         description: '',
+        note1: 'The driver has control and the vehicle can assist with a minor form of automation.',
+        note2: 'Vehicle can provide adaptive cruise control (limited autonomous acceleration/braking) OR lane assist (limited autonomous steering).',
+        note3: 'Radar and/or cameras track the location of surrounding vehicles to provide accurate feedback to the vehicle\'s system.',
         imgSrc: 'https://github.com/katherinemarsh/autonomous-vehicle-infographic/blob/master/src/images/toyotaCorolla2018.png?raw=true',
         imgDesc: '2018 Toyota Corolla with Toyota Safety Sense',
         imgCitation: 'Source: Toyota.'
     },
     {
         num: '2', 
-        description: '',
+        description: 'Some Automation',
+        note1: 'The driver has control and the vehicle can assist with some autonomous functions.',
+        note2: 'Vehicle can provide adaptive cruise control (limited autonomous acceleration/braking) AND lane assist (limited autonomous steering) simultaneously.',
+        note3: 'Radar and/or cameras track the location of surrounding vehicles to provide accurate feedback to the vehicle\'s system.',
         imgSrc: 'https://github.com/katherinemarsh/autonomous-vehicle-infographic/blob/master/src/images/volvoXC40_2019.png?raw=true',
         imgDesc: '2019 Volvo XC40 with Pilot Assist',
         imgCitation: 'Source: Volvo.'
     },
     {
         num: '3', 
-        description: '',
+        description: 'Partial Automation',
         imgSrc: 'https://github.com/katherinemarsh/autonomous-vehicle-infographic/blob/master/src/images/audiA8_2020.png?raw=true',
         imgDesc: '2020 Audi A8',
         imgCitation: 'Source: Audi.'
     },
     {
         num: '4', 
-        description: '',
+        description: 'High Automation',
         imgSrc: 'https://github.com/katherinemarsh/autonomous-vehicle-infographic/blob/master/src/images/waymoMinivan.png?raw=true',
         imgDesc: 'Waymo Chrysler Pacifica Hybrid Minivan',
         imgCitation: 'Source: Waymo.'
     },
     {
         num: '5', 
-        description: '',
+        description: 'Fully Autonomous',
         imgSrc: 'https://github.com/katherinemarsh/autonomous-vehicle-infographic/blob/master/src/images/volkswagonGroupLevel5.png?raw=true',
         imgDesc: 'Future Volkswagon group autonomous vehicle',
         imgCitation: 'Source: Volkswagon.'
